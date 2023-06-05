cc776f0851b2d30cd3dcf4da79e09c08d61a64c5
Neto test <mswadekira@gmail.com>
2023-06-05 07:21:04 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int ticket = 15_482; // стоимость билета
+        int rubles = 20; // для одной бонусной мили
+
+        System.out.println("Купили билет. Начислено милей за билет");
+        System.out.println((ticket / rubles) + " Количество бонусных миль");
+
     }
 }
